6c0c41e1a9537a8b28d05ff1e5e808a5943a8fc6
zbrookle <zachb1996@yahoo.com>
2020-07-02 12:15:14 -0400

    ENH: Testing / linting setup

---
+++ helm-factory/tests/conftest.py
@@ -0,0 +1,12 @@
+import pytest
+from kubernetes import client, config
+
+
+@pytest.fixture(scope="module")
+def load_kubeconfig():
+    config.load_kube_config()
+
+
+@pytest.fixture(scope="module")
+def kube_client():
+    return client.CoreV1Api()

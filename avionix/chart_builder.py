74c34c8366e85b6681a3ad3b35183eb0aa0cbf30
Varun Rao <varun.rao095@gmail.com>
2020-07-12 17:22:52 -0400

    feature: created generate_chart method which will create directory and file structure

--- avionix/chart_builder.py
+++ avionix/chart_builder.py
@@ -1,6 +1,8 @@
 from typing import List
 
 from avionix.kubernetes_objects.base_objects import KubernetesBaseObject
+from avionix.chart_info import ChartInfo
+import os
 
 
 class ChartBuilder:
@@ -9,5 +11,14 @@ class ChartBuilder:
     structure. Can also perform the installation onto the server
     """
 
-    def __init__(self, kubernetes_objects: List[KubernetesBaseObject]):
+    def __init__(self, chart_info: ChartInfo,
+                 kubernetes_objects: List[KubernetesBaseObject] = None):
+        self.chart_info = chart_info
         self.kubernetes_objects = kubernetes_objects
+        self.__templates_directory = f'{self.chart_info.name}/templates'
+        self.__chart_yaml = f'{self.chart_info.name}/chart.yaml'
+
+    def generate_chart(self):
+        os.makedirs(self.__templates_directory, exist_ok=True)
+        with open(self.__chart_yaml, 'w+') as chart_yaml_file:
+            chart_yaml_file.write(str(self.chart_info))

c6a65dd440713c7d701eb796645ca38f736efecd
zbrookle <zachb1996@yahoo.com>
2020-07-15 17:54:22 -0400

    ENH: Add import for maintainer

--- avionix/__init__.py
+++ avionix/__init__.py
@@ -3,4 +3,4 @@ from ._version import get_versions
 __version__ = get_versions()["version"]
 del get_versions
 
-from avionix.chart import ChartBuilder, ChartDependency, ChartInfo
+from avionix.chart import ChartBuilder, ChartDependency, ChartInfo, ChartMaintainer
--- avionix/chart/__init__.py
+++ avionix/chart/__init__.py
@@ -1,3 +1,4 @@
 from avionix.chart.chart_builder import ChartBuilder
 from avionix.chart.chart_dependency import ChartDependency
 from avionix.chart.chart_info import ChartInfo
+from avionix.chart.chart_maintainer import ChartMaintainer

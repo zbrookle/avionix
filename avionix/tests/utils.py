e833bdcd72075b52bdb838ddb4f1e7644016447c
zbrookle <zachb1996@yahoo.com>
2020-07-15 17:22:09 -0400

    ENH: Add parsing output of kubectl

--- avionix/tests/utils.py
+++ avionix/tests/utils.py
@@ -1,3 +1,8 @@
+import re
+from subprocess import check_output
+
+from pandas import DataFrame
+
 from avionix.kubernetes_objects.container import Container
 from avionix.kubernetes_objects.deployment import Deployment, DeploymentSpec
 from avionix.kubernetes_objects.metadata import ObjectMeta
@@ -5,9 +10,11 @@ from avionix.kubernetes_objects.pod import PodSpec, PodTemplateSpec
 from avionix.kubernetes_objects.selector import LabelSelector
 
 
-def get_test_deployment():
+def get_test_deployment(number: int):
     return Deployment(
-        metadata=ObjectMeta(name="test-deployment", labels={"type": "master"}),
+        metadata=ObjectMeta(
+            name=f"test-deployment-{number}", labels={"type": "master"}
+        ),
         spec=DeploymentSpec(
             replicas=1,
             template=PodTemplateSpec(
@@ -19,3 +26,32 @@ def get_test_deployment():
             selector=LabelSelector(match_labels={"container_type": "master"}),
         ),
     )
+
+
+def space_split(output_line: str):
+    return [
+        value
+        for value in re.split(r"(\t|  +)", output_line)
+        if not re.match(r"^\s*$", value)
+    ]
+
+
+def parse_binary_output_to_dict(output: bin):
+    output_lines = output.decode("utf-8").split("\n")
+    names = space_split(output_lines[0])
+    value_rows = []
+    for line in output_lines[1:]:
+        values = space_split(line)
+        value_rows.append(values)
+    print(names)
+    print(value_rows)
+    return DataFrame(data=value_rows, columns=names)
+
+
+def get_helm_installations():
+    output = check_output(["helm", "list"])
+    return parse_binary_output_to_dict(output)
+
+
+def kubectl_get(resource: str):
+    return parse_binary_output_to_dict(check_output(["kubectl", "get", resource]))

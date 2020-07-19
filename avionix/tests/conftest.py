79b0917c5586675125e8f1da383ab2fa02c7cb0f
zbrookle <zachb1996@yahoo.com>
2020-07-19 16:43:22 -0400

    ENH: Make chart_info fixture

--- avionix/tests/conftest.py
+++ avionix/tests/conftest.py
@@ -1,7 +1,9 @@
 import logging
+from pathlib import Path
 
 import pytest
 
+from avionix import ChartInfo
 from avionix.tests.utils import get_test_deployment
 
 logging.basicConfig(format="[%(filename)s: %(lineno)s] %(message)s", level=logging.INFO)
@@ -15,3 +17,15 @@ def test_deployment1():
 @pytest.fixture
 def test_deployment2():
     return get_test_deployment(2)
+
+
+@pytest.fixture
+def chart_info():
+    return ChartInfo(
+        api_version="3.2.4", name="test", version="0.1.0", app_version="v1"
+    )
+
+
+@pytest.fixture(scope="module")
+def test_folder():
+    return Path(__file__).parent

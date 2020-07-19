095673826aabaac7fd059823c3354b374a8f751b
zbrookle <zachb1996@yahoo.com>
2020-07-19 16:43:12 -0400

    ENH: Add error base class

--- avionix/errors.py
+++ avionix/errors.py
@@ -6,17 +6,25 @@ def pre_uninstall_handle_error(message):
         raise ClusterUnavailableError(message)
     if re.match(r"Error: cannot re-use a name that is still in use", message):
         raise ChartAlreadyInstalledError(message)
+    raise NewHelmError(message)
 
 
 def post_uninstall_handle_error(message):
     raise Exception(message)
 
 
-class ClusterUnavailableError(Exception):
+class AvionixError(Exception):
     def __init__(self, msg: str):
         super().__init__(msg)
 
 
-class ChartAlreadyInstalledError(Exception):
-    def __init__(self, msg: str):
-        super().__init__(msg)
+class ClusterUnavailableError(AvionixError):
+    pass
+
+
+class ChartAlreadyInstalledError(AvionixError):
+    pass
+
+
+class HelmError(AvionixError):
+    pass

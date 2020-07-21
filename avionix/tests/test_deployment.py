4af914af8a777dbc189b9f1ff93ebb75b065c2d7
zbrookle <zachb1996@yahoo.com>
2020-07-21 19:48:17 -0400

    ENH: Add unit test for deployment

---
+++ avionix/tests/test_deployment.py
@@ -0,0 +1,27 @@
+import pytest
+
+from avionix import ChartBuilder, ObjectMeta
+from avionix.kubernetes_objects.deployment import Deployment, DeploymentSpec
+from avionix.tests.utils import ChartInstallationContext, kubectl_get
+
+
+@pytest.fixture
+def deployment(test_labels, pod_template_spec, selector):
+    return Deployment(
+        metadata=ObjectMeta(
+            name=f"test-deployment", labels=test_labels
+        ),
+        spec=DeploymentSpec(
+            replicas=1,
+            template=pod_template_spec,
+            selector=selector,
+        ),
+    )
+
+
+def test_deployment(chart_info, test_folder, deployment):
+    builder = ChartBuilder(chart_info, [deployment], test_folder)
+    with ChartInstallationContext(builder):
+        deployment_info = kubectl_get("deployments")
+        assert deployment_info["NAME"][0] == f"test-deployment"
+        assert deployment_info["READY"][0] == "1/1"

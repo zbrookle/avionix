f1603e1c819611ddec72ca12dd10fc4fe45f1af6
zbrookle <zachb1996@yahoo.com>
2020-08-26 19:57:31 -0400

    ENH: Add authentication.py tests

--- avionix/tests/test_authentication.py
+++ avionix/tests/test_authentication.py
@@ -1,9 +1,15 @@
-from avionix.kube.authentication import TokenReview, TokenReviewSpec, TokenRequest, TokenRequestSpec
+from avionix.kube.authentication import (
+    TokenReview,
+    TokenReviewSpec,
+    TokenRequest,
+    TokenRequestSpec,
+)
 from avionix.testing.installation_context import ChartInstallationContext
 from avionix.testing.helpers import kubectl_get, KubectGetException
 from avionix import ChartBuilder, ObjectMeta
 import pytest
 from avionix.errors import HelmError
+
 
 @pytest.mark.xfail(
     raises=KubectGetException, reason="Cannot use kubectl get on " "tokenreview"
@@ -25,12 +31,17 @@ def test_token_review(chart_info):
         assert token_review_info["DESIRED"][0] == "1"
         assert token_review_info["CURRENT"][0] == "1"
 
-@pytest.mark.xfail(
-    raises=HelmError, reason="Helm does not support token request"
-)
+
+@pytest.mark.xfail(raises=HelmError, reason="Helm does not support token request")
 def test_token_request(chart_info):
-    builder = ChartBuilder(chart_info, [TokenRequest(ObjectMeta(
-        name="token-request"), TokenRequestSpec(["audience1", "audience2"], None, None)
-    )])
+    builder = ChartBuilder(
+        chart_info,
+        [
+            TokenRequest(
+                ObjectMeta(name="token-request"),
+                TokenRequestSpec(["audience1", "audience2"], None, None),
+            )
+        ],
+    )
     with ChartInstallationContext(builder):
         print(kubectl_get("tokenrequest"))

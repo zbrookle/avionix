e7db486e3d6d5be79ff5f57b879ccbb3e5359685
zbrookle <zachb1996@yahoo.com>
2020-10-04 13:58:46 -0400

    DOC: Fix the rest of the docstrings

--- avionix/kube/core.py
+++ avionix/kube/core.py
@@ -991,12 +991,13 @@ class TopologySpreadConstraint(HelmYaml):
         matching pods in any two topology domains of a given topology type. For
         example, in a 3-zone cluster, MaxSkew is set to 1, and pods with the same
         labelSelector spread as 1/1/0: | zone1 | zone2 | zone3 | |   P   |   P   |
-          |
-          - if MaxSkew is 1, incoming pod can only be scheduled to zone3 to become
-            1/1/1; scheduling it onto zone1(zone2) would make the ActualSkew(2-0) on
+
+          - if MaxSkew is 1, incoming pod can only be scheduled to zone3 to become \
+            1/1/1; scheduling it onto zone1(zone2) would make the ActualSkew(2-0) on \
             zone1(zone2) violate MaxSkew(1).
-          - if MaxSkew is 2, incoming pod can be scheduled onto any zone. It's a
+          - if MaxSkew is 2, incoming pod can be scheduled onto any zone. It's a \
             required field.
+
         Default value is 1 and 0 is not allowed.
     :param topology_key: TopologyKey is the key of node labels. Nodes that have a label
         with this key and identical values are considered to be in the same topology.
@@ -1004,18 +1005,19 @@ class TopologySpreadConstraint(HelmYaml):
         pods into each bucket. It's a required field.
     :param when_unsatisfiable: WhenUnsatisfiable indicates how to deal with a pod if it
         doesn't satisfy the spread constraint.
-            - DoNotSchedule (default) tells the scheduler not to schedule it
-            - ScheduleAnyway tells the scheduler to still schedule it It's considered as
-                "Unsatisfiable" if and only if placing incoming pod on any topology
-                 violates "MaxSkew". For example, in a 3-zone cluster, MaxSkew is set to
-                 1, and pods with the same labelSelector spread as
-                 3/1/1: | zone1 | zone2 | zone3 | | P P P |   P   |   P   |
 
-         If WhenUnsatisfiable is set to DoNotSchedule, incoming pod can only be
-         scheduled to zone2(zone3) to become 3/2/1(3/1/2) as ActualSkew(2-1) on
-         zone2(zone3) satisfies MaxSkew(1). In other words, the cluster can still be
-         imbalanced, but scheduler won't make it *more* imbalanced. It's a required
-         field.
+            - DoNotSchedule (default) tells the scheduler not to schedule it
+            - ScheduleAnyway tells the scheduler to still schedule it It's considered \
+            as "Unsatisfiable" if and only if placing incoming pod on any topology \
+            violates "MaxSkew". For example, in a 3-zone cluster, MaxSkew is set to \
+            1, and pods with the same labelSelector spread as 3/1/1: | zone1 | zone2 | \
+            zone3 | | P P P |   P   |   P   |
+
+        If WhenUnsatisfiable is set to DoNotSchedule, incoming pod can only be
+        scheduled to zone2(zone3) to become 3/2/1(3/1/2) as ActualSkew(2-1) on
+        zone2(zone3) satisfies MaxSkew(1). In other words, the cluster can still be
+        imbalanced, but scheduler won't make it *more* imbalanced. It's a required
+        field.
     :param label_selector: LabelSelector is used to find matching pods. Pods that match
         this label selector are counted to determine the number of pods in their
         corresponding topology domain.
--- avionix/kube/meta.py
+++ avionix/kube/meta.py
@@ -298,10 +298,12 @@ class StatusCause(HelmYaml):
     """
     :param field: The field of the resource that has caused this error, as named by its
         JSON serialization. May include dot and postfix notation for nested
-        attributes. Arrays are zero-indexed.  Fields may appear more than once in an
+        attributes. Arrays are zero-indexed. Fields may appear more than once in an
         array of causes due to fields having multiple errors. Optional. Examples:
+
             * "name" - the field "name" on the current resource
             * "items[0].name" - the field "name" on the first array entry in "items"
+
     :param message: A human-readable description of the cause of the error.  This field
         may be presented as-is to a reader.
     :param reason: A machine-readable description of the cause of the error. If this
